83a8f4a4c4a5a7811b3186ce2a5b976eb22b005d
Tyrone Usher <tyeusher8@gmail.com>
2021-08-26 20:04:09 -0500

    change

--- server/models/shoe.js
+++ server/models/shoe.js
@@ -1,24 +1,18 @@
 
-
-
-
-
-
-
 //the model of scheme
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const shoeSchema = new mongoose.Schema({//using mongoose to create a new schema
-//   greenLaces: String,
-//   extraSocks: String,
-//   shoePolisher: String,
-//   bottomGripClay: String,
-//   coloredLaces: [String]
-// });
+const shoeSchema = new mongoose.Schema({//using mongoose to create a new schema
+  greenLaces: String,
+  extraSocks: String,
+  shoePolisher: String,
+  bottomGripClay: String,
+  coloredLaces: [String]
+});
 
-// const Shoe = mongoose.model('Shoe', shoeSchema);//shporting out model we just made
+const Shoe = mongoose.model('Shoe', shoeSchema);//shporting out model we just made
 
-// module.exports = {
-//   model: Shoe,
-//   schema: shoeSchema
-// };
+module.exports = {
+  model: Shoe,//exporting the entire model that was created
+  schema: shoeSchema
+};
