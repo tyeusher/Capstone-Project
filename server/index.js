b4111a49b582f0439295afe9a7056bcdf38e91d2
Tyrone Usher <tyeusher8@gmail.com>
2021-09-01 03:51:47 -0500

    d

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
 
 
 
-// app
-//   .route("/")
-//   .get((request, response) => {
-//     response.send(JSON.stringify({message: "No GET routes available on root URI." }), 404);
-//   })
-// .catch(err => console.log(err));
-// const port = process.env.PORT || 4040;
-// app.listen(port, () => console.log(`Listening on port ${port}`));
+app
+  .route("/")
+  .get((request, response) => {
+    response.send(JSON.stringify({message: "No GET routeszc available on root URI." }), 404);
+  })
+.catch(err => console.log(err));
+const port = process.env.PORT || 4040;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// app.route("/").get((request, response) => {
-//   response.send("HELLO WORLD");
-// });
+app.route("/").get((request, response) => {
+  response.send("HELLO WORLD");
+});
 
