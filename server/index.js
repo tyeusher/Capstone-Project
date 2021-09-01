864653feedebf02c4b13fb303ccd617bcf13082f
Tyrone Usher <tyeusher8@gmail.com>
2021-09-01 04:03:58 -0500

    ch

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app
   .get((request, response) => {
     response.send(JSON.stringify({message: "No GET routeszc available on root URI." }), 404);
   })
-.catch(err => console.log(err));
+// .catch(err => console.log(err));
 const port = process.env.PORT || 4040;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -17,3 +17,49 @@ app.route("/").get((request, response) => {
   response.send("HELLO WORLD");
 });
 
+app.route("/homes").get((request, response)=>{
+    response.send(JSON.stringify(homes))
+})
+
+
+let homes =[
+    {
+        id: 8,
+        price: 1,
+        beds: 4,
+        bath: 2
+    },
+    {
+        id: 2,
+        price: 5,
+        beds: 7,
+        bath: 2
+    },
+  {
+        id: 3,
+        price: 4,
+        beds: 6,
+        bath: 2
+    },
+  
+    {
+        id: 4,
+        price: 4,
+        beds: 3,
+        bath: 1
+    },
+  
+    {
+        id: 5,
+        price: 7,
+        beds: 6,
+        bath: 3
+    },
+  
+    {
+        id: 6,
+        price: 4,
+        beds: 3,
+        bath: 2
+    }
+  ]
